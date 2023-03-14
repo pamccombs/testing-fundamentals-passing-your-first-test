7765b6fd13a0ebfe5d9fd91c070277be7d10d727
P. Reese McCombs <34119196+pamccombs@users.noreply.github.com>
2023-03-14 17:37:47 +0000

    Add test to full name method

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,8 +4,9 @@ describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
       # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new(first_name: 'John', last_name: 'Doe')
+      expect(person.full_name).to eq('John Doe')
+      # You can delete the pending line.  
     end
   end
 end
